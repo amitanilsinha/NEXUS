b19cfc6db4b3abcc5d96059c4d3805430abdf411
amitanilsinha <43570792+amitanilsinha@users.noreply.github.com>
2018-11-12 22:39:15 +0530

    Update Create_RG.ps1

--- Scripts/Create_RG.ps1
+++ Scripts/Create_RG.ps1
@@ -1,52 +1,69 @@
-# Create a network security group
-$nsg = New-AzureRmNetworkSecurityGroup -ResourceGroupName $resourceGroup -Location $location `
-  -Name myNetworkSecurityGroup -SecurityRules $nsgRuleSSH
+Connect-AzureRmAccount
+#You need an SSH key pair to complete this quickstart. If you already have an SSH key pair, you can skip this step.
+#ssh-keygen -t rsa -b 2048
+#Variables
+$ResourceGroup="NEWNEXUSRG"
+$Location="centralindia"
+#Create an Azure resource group
+New-AzureRmResourceGroup -Name $ResourceGroup -Location $Location
 
-# Create an inbound network security group rule for port 80
-$httprule = New-AzureRmNetworkSecurityRuleConfig `
-    -Name "myNetworkSecurityGroupRule" `
-    -Description "Allow HTTP" `
-    -Access "Allow" `
-    -Protocol "Tcp" `
-    -Direction "Inbound" `
-    -Priority "100" `
-    -SourceAddressPrefix "Internet" `
-    -SourcePortRange * `
-    -DestinationAddressPrefix * `
-    -DestinationPortRange 80
+# Create a subnet configuration
+$subnetConfig = New-AzureRmVirtualNetworkSubnetConfig `
+  -Name "mySubnet" `
+  -AddressPrefix 192.168.1.0/24
 
-$nsg = New-AzureRmNetworkSecurityGroup `
-    -ResourceGroupName $resourceGroup `
-    -Location $Location `
-    -Name "myNetworkSecurityGroup" `
-    -SecurityRules $httprule
+# Create a virtual network
+$vnet = New-AzureRmVirtualNetwork `
+  -ResourceGroupName $ResourceGroup `
+  -Location $Location `
+  -Name "myVNET" `
+  -AddressPrefix 192.168.0.0/16 `
+  -Subnet $subnetConfig
 
-$vnet = Get-AzureRmVirtualNetwork `
-    -ResourceGroupName $resourceGroup `
-    -Name "MYvNET"
-
-$subnetPrefix = $vnet.Subnets|?{$_.Name -eq 'mySubnet'}
-Set-AzureRmVirtualNetworkSubnetConfig `
-    -VirtualNetwork $vnet `
-    -Name "mySubnet" `
-    -AddressPrefix $subnetPrefix.AddressPrefix `
-    -NetworkSecurityGroup $nsg
-
-Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
+# Create a public IP address and specify a DNS name
+$pip = New-AzureRmPublicIpAddress `
+  -ResourceGroupName  $ResourceGroup `
+  -Location $Location `
+  -AllocationMethod Static `
+  -IdleTimeoutInMinutes 4 `
+  -Name "mypublicdns$(Get-Random)"
 
 # Create an inbound network security group rule for port 443
-$httprule = New-AzureRmNetworkSecurityRuleConfig `
-    -Name "myNetworkSecurityGroupRule" `
-    -Description "Allow HTTP" `
-    -Access "Allow" `
-    -Protocol "Tcp" `
-    -Direction "Inbound" `
-    -Priority "100" `
-    -SourceAddressPrefix "Internet" `
-    -SourcePortRange * `
-    -DestinationAddressPrefix * `
-    -DestinationPortRange 443
+$nsgRuleSSH = New-AzureRmNetworkSecurityRuleConfig `
+  -Name "myNetworkSecurityGroupRuleSSH"  `
+  -Protocol "Tcp" `
+  -Direction "Inbound" `
+  -Priority 1000 `
+  -SourceAddressPrefix * `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
+  -DestinationPortRange 443 `
+  -Access "Allow"
+
+# Create an inbound network security group rule for port 80
+$nsgRuleWeb = New-AzureRmNetworkSecurityRuleConfig `
+  -Name "myNetworkSecurityGroupRuleWWW"  `
+  -Protocol "Tcp" `
+  -Direction "Inbound" `
+  -Priority 1001 `
+  -SourceAddressPrefix * `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
+  -DestinationPortRange 80 `
+  -Access "Allow"
+
+# Create a network security group
+$nsg = New-AzureRmNetworkSecurityGroup `
+  -ResourceGroupName $ResourceGroup `
+  -Location $Location `
+  -Name "myNetworkSecurityGroup" `
+  -SecurityRules $nsgRuleSSH,$nsgRuleWeb
 
 # Create a virtual network card and associate with public IP address and NSG
-$nic = New-AzureRmNetworkInterface -Name myNic -ResourceGroupName $resourceGroup -Location $location `
-  -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
+$nic = New-AzureRmNetworkInterface `
+  -Name "myNic" `
+  -ResourceGroupName $ResourceGroup `
+  -Location $Location `
+  -SubnetId $vnet.Subnets[0].Id `
+  -PublicIpAddressId $pip.Id `
+  -NetworkSecurityGroupId $nsg.Id
