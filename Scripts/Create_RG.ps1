03a52697520af13772e38436aa3f7f5439028f96
amitanilsinha <43570792+amitanilsinha@users.noreply.github.com>
2018-11-12 16:07:21 +0530

    Create Create_RG.ps1

---
+++ Scripts/Create_RG.ps1
@@ -0,0 +1,52 @@
+# Create a network security group
+$nsg = New-AzureRmNetworkSecurityGroup -ResourceGroupName $resourceGroup -Location $location `
+  -Name myNetworkSecurityGroup -SecurityRules $nsgRuleSSH
+
+# Create an inbound network security group rule for port 80
+$httprule = New-AzureRmNetworkSecurityRuleConfig `
+    -Name "myNetworkSecurityGroupRule" `
+    -Description "Allow HTTP" `
+    -Access "Allow" `
+    -Protocol "Tcp" `
+    -Direction "Inbound" `
+    -Priority "100" `
+    -SourceAddressPrefix "Internet" `
+    -SourcePortRange * `
+    -DestinationAddressPrefix * `
+    -DestinationPortRange 80
+
+$nsg = New-AzureRmNetworkSecurityGroup `
+    -ResourceGroupName $resourceGroup `
+    -Location $Location `
+    -Name "myNetworkSecurityGroup" `
+    -SecurityRules $httprule
+
+$vnet = Get-AzureRmVirtualNetwork `
+    -ResourceGroupName $resourceGroup `
+    -Name "MYvNET"
+
+$subnetPrefix = $vnet.Subnets|?{$_.Name -eq 'mySubnet'}
+Set-AzureRmVirtualNetworkSubnetConfig `
+    -VirtualNetwork $vnet `
+    -Name "mySubnet" `
+    -AddressPrefix $subnetPrefix.AddressPrefix `
+    -NetworkSecurityGroup $nsg
+
+Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
+
+# Create an inbound network security group rule for port 443
+$httprule = New-AzureRmNetworkSecurityRuleConfig `
+    -Name "myNetworkSecurityGroupRule" `
+    -Description "Allow HTTP" `
+    -Access "Allow" `
+    -Protocol "Tcp" `
+    -Direction "Inbound" `
+    -Priority "100" `
+    -SourceAddressPrefix "Internet" `
+    -SourcePortRange * `
+    -DestinationAddressPrefix * `
+    -DestinationPortRange 443
+
+# Create a virtual network card and associate with public IP address and NSG
+$nic = New-AzureRmNetworkInterface -Name myNic -ResourceGroupName $resourceGroup -Location $location `
+  -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
