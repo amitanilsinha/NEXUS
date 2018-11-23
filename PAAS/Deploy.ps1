d39aa9186228994a857af7d18a935e95f47fe29b
amitanilsinha <43570792+amitanilsinha@users.noreply.github.com>
2018-11-23 14:19:03 +0530

    Update Deploy.ps1

--- PAAS/Deploy.ps1
+++ PAAS/Deploy.ps1
@@ -2,11 +2,10 @@ $RG = "testinart"
 $location = "East US"
 New-AzureRmResourceGroup -Name $RG -Location $location
 New-AzureRmResourceGroupDeployment -Name Nexusaspaas -ResourceGroupName $RG `
- -TemplateUri "https://raw.githubusercontent.com/amitanilsinha/NEXUS/master/PAAS/Nexusinfra.json" `
+ -TemplateUri "https://raw.githubusercontent.com/amitanilsinha/NEXUS/master/PAAS/nexusinfra.json" `
 -TemplateParameterUri "https://raw.githubusercontent.com/amitanilsinha/NEXUS/master/PAAS/Nexusinfraparameter.json"
 
 $ipname = (Get-AzureRmResource  -ResourceGroupName $RG  -ResourceType Microsoft.Network/publicIPAddresses).Name[1]
 $IP = (Get-AzureRmPublicIpAddress -Name $ipname -ResourceGroupName $RG).IpAddress
 Write-Host "Login from browser with $IP and port 8080" -ForegroundColor Green 
 Write-Host "Login Username is admin and Password is admin123" -ForegroundColor Green
-               
