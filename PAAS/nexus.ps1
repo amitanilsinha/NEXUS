e49e26182a5aa9ebe1e3f6e17bb4a41d3ba90f67
amitanilsinha <43570792+amitanilsinha@users.noreply.github.com>
2018-11-23 12:13:41 +0530

    Update nexus.ps1

--- PAAS/nexus.ps1
+++ PAAS/nexus.ps1
@@ -49,101 +49,27 @@ Configuration Installartifactory {
       Configuration ContosoWebsite
 {
   
-  #Install the IIS Role
-          WindowsFeature IIS
-         {
-          Ensure = “Present”
-          Name = “Web-Server”
-         }
+  {
+    #Install the IIS Role
+    WindowsFeature IIS
+    {
+      Ensure = “Present”
+      Name = “Web-Server”
+    }
 
-         #Install ASP.NET 4.5
-         WindowsFeature ASP
-         {
-          Ensure = “Present”
-          Name = “Web-Asp-Net45”
-         }
+    #Install ASP.NET 4.5
+    WindowsFeature ASP
+    {
+      Ensure = “Present”
+      Name = “Web-Asp-Net45”
+    }
 
-         WindowsFeature WebServerManagementConsole
-         {
-          Name = "Web-Mgmt-Console"
-          Ensure = "Present"
-         }
-         File devfolder
-         {
-            Type = 'Directory'
-            DestinationPath = 'C:\inetpub\wwwroot\dev'
-            Ensure = "Present"
-            DependsOn       = '[WindowsFeature]ASP'
-         }
-         File uatfolder
-         {
-            Type = 'Directory'
-            DestinationPath = 'C:\inetpub\wwwroot\uat'
-            Ensure = "Present"
-            DependsOn       = '[WindowsFeature]ASP'
-         }  
-         File prodfolder
-         {
-            Type = 'Directory'
-            DestinationPath = 'C:\inetpub\wwwroot\prod'
-            Ensure = "Present"
-            DependsOn       = '[WindowsFeature]ASP'
-         }
-
-          xWebsite DevWebsite
-         {
-            Ensure          = 'Present'
-            Name            = $WebSitePrefix + '-dev'
-            State           = 'Started'
-            PhysicalPath    = 'C:\inetpub\wwwroot\dev'
-            BindingInfo     = @( MSFT_xWebBindingInformation
-                                 {
-                                   Protocol              = "HTTP"
-                                   Port                  = 80
-                                   HostName = $DevPublicDNS
-                                 }
-
-                                )
-            DependsOn       = '[File]devfolder'
-
-         } 
-         xWebsite UatWebsite
-         {
-            Ensure          = 'Present'
-            Name            = $WebSitePrefix +'-uat'
-            State           = 'Started'
-            PhysicalPath    = 'C:\inetpub\wwwroot\uat'
-            BindingInfo     = @( MSFT_xWebBindingInformation
-                                 {
-                                   Protocol              = "HTTP"
-                                   Port                  = 80
-                                   HostName = $UatPublicDNS
-                                 }
-
-                                )
-            DependsOn       = '[File]uatfolder'
-         }
-
-         xWebsite prodWebsite
-         {
-            Ensure          = 'Present'
-            Name            = $WebSitePrefix +'-prod'
-            State           = 'Started'
-            PhysicalPath    = 'C:\inetpub\wwwroot\prod'
-            BindingInfo     = @( MSFT_xWebBindingInformation
-                                 {
-                                   Protocol              = "HTTP"
-                                   Port                  = 80
-                                   HostName = $ProdPublicDNS
-                                 }
-
-                                )
-            DependsOn       = '[File]prodfolder'
-         }
-
+     WindowsFeature WebServerManagementConsole
+    {
+        Name = "Web-Mgmt-Console"
+        Ensure = "Present"
+    }
   }
-} 
-
 } 
       Archive nexus {  
         Ensure = 'Present'  
